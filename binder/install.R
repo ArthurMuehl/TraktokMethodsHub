480eac538e81ba2427f3084f932aa8b6e516dc62
Arthur Mühl <arthur.muehl@gesis.org>
2026-05-19 15:38:44 +0200

    Update install.R

--- binder/install.R
+++ binder/install.R
@@ -1,4 +1,4 @@
-install.packages("chromote")
-install.packages('traktok', repos = c('https://jbgruber.r-universe.dev', 'https://cloud.r-project.org'))
+install.packages("chromote", repos = "https://cloud.r-project.org")
+install.packages("traktok",
+  repos = c("https://jbgruber.r-universe.dev", "https://cloud.r-project.org"))
 install.packages("cookiemonster")
-
